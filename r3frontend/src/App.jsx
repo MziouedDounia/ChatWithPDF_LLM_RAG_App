87a45fd69a8c73300c2e2d92981ccc8c3099caba
MziouedDounia <d.mzioued0@gmail.com>
2024-09-12 13:44:12 +0100

    avatar+tts

--- r3frontend/src/App.jsx
+++ r3frontend/src/App.jsx
@@ -6,13 +6,14 @@ import "./styles.css";
 function App() {
   return (
     <>
-      <div className="z-10 md:justify-center fixed bottom-4 left-4 right-4 flex gap-3 flex-wrap justify-stretch">
-            <TypingBox />
-      </div>
+      <div className="typing-box-container">
+        <TypingBox/>
+        </div>
       <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
         <color attach="background" args={["#ececec"]} />
         <Experience />
       </Canvas>
+      
     </>
   );
 }
